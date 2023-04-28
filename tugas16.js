cd264dc57c18093cc752c4b38ff8f90fbd5bf404
Yehezkiel Reynaldi Abraham <yehezkielreynaldi01@gmail.com>
2023-04-28 19:07:19 +0700

    Tugas 16 JS
    Tugas ini saya kirimkan untuk Niomic

---
+++ tugas16.js
@@ -0,0 +1,10 @@
+function cariNilaiMinMax(){
+
+    let min = (Math.min(45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10));
+    console.log(`Nilai Minimal : ${min}`);
+    
+    let max = (Math.max(45, 100, 99, 3, 2, 81, 22, 55, 1, 2, 3, 4, 5));
+    console.log(`Nilai Maksimal : ${max}`);
+}
+
+cariNilaiMinMax();
